edcea321cd28a71e7de480d555e26348dbaccb2d
Andrija Milovanovic <andrija@streamsoft.rs>
2021-08-20 12:03:12 +0200

    Refactoring in-memory cache
    Adding thread safe dictionary structure for in-memory cache.

--- client/api/analytics/AnalyticsPublisherService.cs
+++ client/api/analytics/AnalyticsPublisherService.cs
@@ -49,7 +49,7 @@ namespace io.harness.cfsdk.client.api.analytics
         {
             Log.Information("Reading from queue and building cache, SDL version: " + sdkVerion);
 
-            Dictionary<Analytics, int> all = analyticsCache.GetAllElements();
+            IDictionary<Analytics, int> all = analyticsCache.GetAllElements();
 
             if (all.Count != 0)
             {
@@ -61,8 +61,8 @@ namespace io.harness.cfsdk.client.api.analytics
                     {
                         DateTime startTime = DateTime.Now;
                         HarnessOpenMetricsAPIService.Client client = new HarnessOpenMetricsAPIService.Client(metricsAPI.httpClient);
-                        await client.MetricsAsync(environmentID, cluster, metrics);
                         Log.Information("Trying to send --->  {Eid} ----- {@mb}", environmentID, metrics);
+                        await client.MetricsAsync(environmentID, cluster, metrics);
                         DateTime endTime = DateTime.Now;
                         if ((endTime - startTime).TotalMilliseconds > config.MetricsServiceAcceptableDuration)
                         {
@@ -84,7 +84,7 @@ namespace io.harness.cfsdk.client.api.analytics
             }
         }
 
-        private Metrics prepareMessageBody(Dictionary<Analytics, int> all)
+        private Metrics prepareMessageBody(IDictionary<Analytics, int> all)
         {
             Metrics metrics = new Metrics();
             metrics.TargetData = new List<TargetData>();
