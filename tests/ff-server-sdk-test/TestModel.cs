aab46bc9f8b60c6de4e218d9c7ddb7a41af78613
Andrija Milovanovic <andrija@streamsoft.rs>
2022-01-11 17:28:08 +0100

    Test project

---
+++ tests/ff-server-sdk-test/TestModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using io.harness.cfsdk.HarnessOpenAPIService;
+
+namespace ff_server_sdk_test
+{
+
+
+    public class TestModel
+    {
+        public string testFile;
+        public FeatureConfig flag;
+        public List<io.harness.cfsdk.client.dto.Target> targets;
+        public List<Segment> segments;
+        public Dictionary<string, bool> expected;
+        public TestModel()
+        {
+        }
+    }
+
+    public class TestResult
+    {
+        public TestModel TestModel { get; set; }
+        public string Identifier { get; set;  }
+        public bool Result { get; set; }
+    }
+}
