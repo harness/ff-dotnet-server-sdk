a94a0e76383d2d157c545b60658595f319dab5b7
Andrija Milovanovic <andrija@streamsoft.rs>
2022-01-26 01:21:09 +0100

    Sample application update

--- client/api/FileMapStore.cs
+++ client/api/FileMapStore.cs
@@ -1,38 +1,48 @@
 ﻿using System;
+using System.Linq;
+using System.IO;
 using System.Collections.Generic;
 using io.harness.cfsdk.client.cache;
+using Newtonsoft.Json;
 
 namespace io.harness.cfsdk.client.api
 {
     public class FileMapStore : IStore
     {
+        private string storeName;
         public FileMapStore(string name)
         {
+            
+            storeName = name;
+            Directory.CreateDirectory(name);
+            Array.ForEach(Directory.EnumerateFiles(name).ToArray(), f => File.Delete(f));
         }
 
         public void Close()
         {
-            throw new NotImplementedException();
+            
         }
 
         public void Delete(string key)
         {
-            throw new NotImplementedException();
+            File.Delete(Path.Combine(storeName, key));
         }
 
         public object Get(string key)
         {
-            throw new NotImplementedException();
+            var str = File.ReadAllText(Path.Combine(storeName, key));
+            return JsonConvert.DeserializeObject(str);
         }
 
         public ICollection<string> Keys()
         {
-            throw new NotImplementedException();
+            return Directory.EnumerateFiles(storeName).Select( f => Path.GetFileName(f)).ToList();
         }
 
         public void Set(string key, object value)
         {
-            throw new NotImplementedException();
+            var str = JsonConvert.SerializeObject(value);
+            File.WriteAllText(Path.Combine(storeName, key), str);
         }
     }
 }
