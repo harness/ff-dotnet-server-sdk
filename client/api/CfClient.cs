d70388e7b709159f3fa7b307dd9a33f914d74c30
Andrija Milovanovic <andrija@streamsoft.rs>
2022-01-28 12:12:35 +0100

    Interface update to have parity with previous release

--- client/api/CfClient.cs
+++ client/api/CfClient.cs
@@ -9,10 +9,12 @@ namespace io.harness.cfsdk.client.api
 {
     public interface ICfClient
     {
-        void Initialize(string apiKey);
-        void Initialize(string apiKey, Config config);
-        void Initialize(IConnector connector);
-        void Initialize(IConnector connector, Config config);
+        Task Initialize(string apiKey);
+        Task Initialize(string apiKey, Config config);
+        Task Initialize(IConnector connector);
+        Task Initialize(IConnector connector, Config config);
+
+        Task WaitToInitialize();
 
         bool BoolVariation(string key, dto.Target target, bool defaultValue);
         string StringVariation(string key, dto.Target target, string defaultValue);
@@ -23,8 +25,6 @@ namespace io.harness.cfsdk.client.api
         IDisposable Subscribe(NotificationType evn, IObserver<Event> observer);
         void Update(Message msg);
 
-        Task StartAsync();
-
         void Close();
     }
 
@@ -34,24 +34,50 @@ namespace io.harness.cfsdk.client.api
         private static readonly Lazy<CfClient> lazy = new Lazy<CfClient>(() => new CfClient());
         public static ICfClient Instance { get { return lazy.Value; } }
 
-        private InnerClient client = null;
+        private readonly InnerClient client = null;
 
-
-        public CfClient() { }
         // alternative client creation
         public CfClient(string apiKey) : this(apiKey, Config.Builder().Build()) {}
         public CfClient(IConnector connector) : this(connector, Config.Builder().Build()) { }
-        public CfClient(string apiKey, Config config) { client = new InnerClient(apiKey, config); }
-        public CfClient(IConnector connector, Config config) { client = new InnerClient(connector, config);  }
+        public CfClient()
+        {
+            client = new InnerClient();
+        }
+        public CfClient(string apiKey, Config config)
+        {
+            client = new InnerClient(apiKey, config);
+            client.Start();
+        }
+        public CfClient(IConnector connector, Config config)
+        {
+            client = new InnerClient(connector, config);
+            client.Start();
+        }
+        // start authentication with server
+        public async Task WaitToInitialize()
+        {
+            await client.WaitToInitialize();
+        }
 
         // initialize singletone instance
-        public void Initialize(string apiKey) { Initialize(apiKey, Config.Builder().Build());  }
-        public void Initialize(IConnector connector) { Initialize(connector, Config.Builder().Build());  }
-        public void Initialize(string apiKey, Config config) { client = new InnerClient(apiKey, config); }
-        public void Initialize(IConnector connector, Config config){ client = new InnerClient(connector, config); }
-
-        // start authentication with server
-        public async Task StartAsync() { await client.StartAsync(); }
+        public async Task Initialize(string apiKey)
+        {
+            await Initialize(apiKey, Config.Builder().Build());
+        }
+        public async Task Initialize(IConnector connector)
+        {
+           await Initialize(connector, Config.Builder().Build());
+        }
+        public async Task Initialize(string apiKey, Config config)
+        {
+            client.Initialize(apiKey, config);
+            await client.StartAsync();
+        }
+        public async Task Initialize(IConnector connector, Config config)
+        {
+            client.Initialize(connector, config);
+            await client.StartAsync();
+        }
 
         // read values
         public bool BoolVariation(string key, dto.Target target, bool defaultValue) { return client.BoolVariation(key, target, defaultValue);  }
