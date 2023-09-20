a5f57b9cfa614c0b5b248596b19a39f1d585b881
Andrew Bell <115623869+andybharness@users.noreply.github.com>
2023-09-20 17:22:14 +0100

    change script to only build the SDK

--- build.sh
+++ build.sh
@@ -8,7 +8,9 @@ if [ "$DOTNET_VERSION" -lt 7 ]; then
 	exit 1
 fi
 
-dotnet pack
+set -x
+
+dotnet pack ff-netF48-server-sdk.csproj
 
 # Install Tools needed for build
 dotnet tool install --global coverlet.console --version 3.2.0
@@ -17,13 +19,13 @@ dotnet tool restore
 
 # Install Libraries needed for build and buld
 dotnet add package JUnitTestLogger --version 1.1.0 
-dotnet restore
-dotnet build --no-restore
+dotnet restore ff-netF48-server-sdk.csproj
+dotnet build ff-netF48-server-sdk.csproj --no-restore
 
 
 # Run tests
 echo "Generating Test Report"
 export MSBUILDDISABLENODEREUSE=1
-dotnet test -v=n --blame-hang --logger:"junit;LogFilePath=junit.xml" -nodereuse:false
+dotnet test tests/ff-server-sdk-test/ff-server-sdk-test.csproj -v=n --blame-hang --logger:"junit;LogFilePath=junit.xml" -nodereuse:false
 ls -l
 echo "Done"
