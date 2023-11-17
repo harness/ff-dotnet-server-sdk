bcc197845f730219d1729b57a824c324d6bacd98
Andrew Bell <115623869+andybharness@users.noreply.github.com>
2023-11-17 15:09:27 +0000

    add net461 as recommended by https://learn.microsoft.com/en-us/dotnet/standard/library-guidance/cross-platform-targeting

--- client/connector/HarnessConnector.cs
+++ client/connector/HarnessConnector.cs
@@ -55,7 +55,7 @@ namespace io.harness.cfsdk.client.connector
                 return new HttpClient();
             }
 
-#if NETSTANDARD
+#if (NETSTANDARD || NET461)
             throw new NotSupportedException("Custom TLS certificates require .net5.0 target or greater");
 #else
             var logger = loggerFactory.CreateLogger<HarnessConnector>();
