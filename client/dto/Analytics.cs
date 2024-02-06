8072338c7b18a18ed87cce9b666564084cbffb74
Erdi Rowlands <erdirowlands@gmail.com>
2024-02-06 19:55:22 +0000

    FFM-10602 Add beginning of Analytics cache refactor

--- client/dto/Analytics.cs
+++ client/dto/Analytics.cs
@@ -4,43 +4,130 @@ using io.harness.cfsdk.HarnessOpenAPIService;
 
 namespace io.harness.cfsdk.client.dto
 {
-    public class Analytics : IEquatable<Analytics>
+// Base Analytics class
+    public abstract class Analytics : IEquatable<Analytics>
     {
-        private readonly FeatureConfig featureConfig;
-        private readonly Target target;
-        private readonly Variation variation;
+        protected readonly Target target;
 
-        public FeatureConfig FeatureConfig { get => featureConfig; }
-        public Target Target { get => target; }
-        public Variation Variation { get => variation; }
-
-        public Analytics(FeatureConfig featureConfig, Target target, Variation variation, EventType eventType)
+        protected Analytics(Target target)
         {
-            this.featureConfig = featureConfig;
             this.target = target;
-            this.variation = variation;
         }
+
+        public Target Target => target;
+        public abstract bool Equals(Analytics other);
 
         public override bool Equals(object obj)
         {
             return Equals(obj as Analytics);
         }
 
-        public bool Equals(Analytics other)
+        public abstract override int GetHashCode();
+    }
+
+// EvaluationAnalytics subclass
+    public class EvaluationAnalytics : Analytics
+    {
+        // The global target is used when we don't want to use the actual target in the evaluation metrics
+        // payload.  Since 1.4.2 the global target has been used.
+        private static readonly string GlobalTargetIdentifier = "__global__cf_target";
+        private static readonly string GlobalTargetName = "Global Target";
+        private readonly FeatureConfig featureConfig;
+        private readonly Variation variation;
+
+        public EvaluationAnalytics(FeatureConfig featureConfig, Variation variation, Target target)
+            : base(target)
         {
-            return other != null &&
-                   EqualityComparer<FeatureConfig>.Default.Equals(featureConfig, other.featureConfig) &&
-                   EqualityComparer<Target>.Default.Equals(target, other.target) &&
-                   EqualityComparer<Variation>.Default.Equals(variation, other.variation);
+            this.featureConfig = featureConfig;
+            this.variation = variation;
+        }
+
+
+        public override bool Equals(Analytics other)
+        {
+            return other is EvaluationAnalytics otherEvaluation
+                   && EqualityComparer<FeatureConfig>.Default.Equals(featureConfig, otherEvaluation.featureConfig)
+                   && EqualityComparer<Variation>.Default.Equals(variation, otherEvaluation.variation);
         }
 
         public override int GetHashCode()
         {
-            int hashCode = -1526478203;
+            var hashCode = -1526478203;
             hashCode = hashCode * -1521134295 + EqualityComparer<FeatureConfig>.Default.GetHashCode(featureConfig);
-            hashCode = hashCode * -1521134295 + EqualityComparer<Target>.Default.GetHashCode(target);
             hashCode = hashCode * -1521134295 + EqualityComparer<Variation>.Default.GetHashCode(variation);
             return hashCode;
         }
+
+        // Additional methods...
     }
+
+// TargetAnalytics subclass
+    public class TargetAnalytics : Analytics
+    {
+        private readonly Target target;
+
+        // Properties...
+
+        public TargetAnalytics(Target target)
+            : base(target)
+        {
+            // Initialization...
+        }
+
+        public override bool Equals(Analytics other)
+        {
+            return other is TargetAnalytics otherTarget
+                   && EqualityComparer<Target>.Default.Equals(target, otherTarget.target);
+        }
+
+        public override int GetHashCode()
+        {
+            return EqualityComparer<Target>.Default.GetHashCode(target);
+        }
+
+        // Additional methods...
+    }
+
+// Usage in caches and other operations remains the same
+
+
+    // public class Analytics : IEquatable<Analytics>
+    // {
+    //     private readonly FeatureConfig featureConfig;
+    //     private readonly Target target;
+    //     private readonly Variation variation;
+    //
+    //     public FeatureConfig FeatureConfig { get => featureConfig; }
+    //     public Target Target { get => target; }
+    //     public Variation Variation { get => variation; }
+    //
+    //     public Analytics(FeatureConfig featureConfig, Target target, Variation variation, EventType eventType)
+    //     {
+    //         this.featureConfig = featureConfig;
+    //         this.target = target;
+    //         this.variation = variation;
+    //     }
+    //
+    //     public override bool Equals(object obj)
+    //     {
+    //         return Equals(obj as Analytics);
+    //     }
+    //
+    //     public bool Equals(Analytics other)
+    //     {
+    //         return other != null &&
+    //                EqualityComparer<FeatureConfig>.Default.Equals(featureConfig, other.featureConfig) &&
+    //                EqualityComparer<Target>.Default.Equals(target, other.target) &&
+    //                EqualityComparer<Variation>.Default.Equals(variation, other.variation);
+    //     }
+    //
+    //     public override int GetHashCode()
+    //     {
+    //         int hashCode = -1526478203;
+    //         hashCode = hashCode * -1521134295 + EqualityComparer<FeatureConfig>.Default.GetHashCode(featureConfig);
+    //         hashCode = hashCode * -1521134295 + EqualityComparer<Target>.Default.GetHashCode(target);
+    //         hashCode = hashCode * -1521134295 + EqualityComparer<Variation>.Default.GetHashCode(variation);
+    //         return hashCode;
+    //     }
+    // }
 }
