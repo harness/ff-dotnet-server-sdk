39f6a4ec3e2add4d5b777e923fa4b7fe3d3c4ce9
Erdi Rowlands <erdirowlands@gmail.com>
2024-02-07 17:08:33 +0000

    FFM-10602 Tidyup

--- client/dto/Analytics.cs
+++ client/dto/Analytics.cs
@@ -62,8 +62,6 @@ namespace io.harness.cfsdk.client.dto
             hashCode = hashCode * -1521134295 + EqualityComparer<Variation>.Default.GetHashCode(Variation);
             return hashCode;
         }
-
-        // Additional methods...
     }
 
 // TargetAnalytics subclass
@@ -84,50 +82,5 @@ namespace io.harness.cfsdk.client.dto
         {
             return EqualityComparer<Target>.Default.GetHashCode(target);
         }
-
-        // Additional methods...
     }
-
-// Usage in caches and other operations remains the same
-
-
-    // public class Analytics : IEquatable<Analytics>
-    // {
-    //     private readonly FeatureConfig featureConfig;
-    //     private readonly Target target;
-    //     private readonly Variation variation;
-    //
-    //     public FeatureConfig FeatureConfig { get => featureConfig; }
-    //     public Target Target { get => target; }
-    //     public Variation Variation { get => variation; }
-    //
-    //     public Analytics(FeatureConfig featureConfig, Target target, Variation variation, EventType eventType)
-    //     {
-    //         this.featureConfig = featureConfig;
-    //         this.target = target;
-    //         this.variation = variation;
-    //     }
-    //
-    //     public override bool Equals(object obj)
-    //     {
-    //         return Equals(obj as Analytics);
-    //     }
-    //
-    //     public bool Equals(Analytics other)
-    //     {
-    //         return other != null &&
-    //                EqualityComparer<FeatureConfig>.Default.Equals(featureConfig, other.featureConfig) &&
-    //                EqualityComparer<Target>.Default.Equals(target, other.target) &&
-    //                EqualityComparer<Variation>.Default.Equals(variation, other.variation);
-    //     }
-    //
-    //     public override int GetHashCode()
-    //     {
-    //         int hashCode = -1526478203;
-    //         hashCode = hashCode * -1521134295 + EqualityComparer<FeatureConfig>.Default.GetHashCode(featureConfig);
-    //         hashCode = hashCode * -1521134295 + EqualityComparer<Target>.Default.GetHashCode(target);
-    //         hashCode = hashCode * -1521134295 + EqualityComparer<Variation>.Default.GetHashCode(variation);
-    //         return hashCode;
-    //     }
-    // }
 }
