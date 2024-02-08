6892184b4c94d040f12fa3884e0c3f3b2ea45dee
Erdi Rowlands <erdirowlands@gmail.com>
2024-02-08 21:15:09 +0000

    FFM-10602 Address PR comment, plus only log warning once per interval
    FFM-10602 Address PR comment, plus only log warning once per interval
    
    FFM-10602 Address PR comment, plus only log warning once per interval
    
    FFM-10602 Address PR comment, plus only log warning once per interval

--- client/api/analytics/AnalyticsManager.cs
+++ client/api/analytics/AnalyticsManager.cs
@@ -16,6 +16,7 @@ namespace io.harness.cfsdk.client.api.analytics
         private Timer timer;
         
         private bool isGlobalTargetEnabled;
+        private bool warningLoggedForInterval = false;
 
         public MetricsProcessor(Config config, AnalyticsCache analyticsCache,
             AnalyticsPublisherService analyticsPublisherService, ILoggerFactory loggerFactory, bool globalTargetEnabled)
@@ -56,48 +57,73 @@ namespace io.harness.cfsdk.client.api.analytics
             var cacheSize = analyticsCache.GetAllElements().Count;
             var bufferSize = config.bufferSize;
 
-            if (cacheSize > bufferSize)
+            if (isGlobalTargetEnabled)
             {
-                logger.LogWarning(
-                    "Metric frequency map exceeded buffer size ({cacheSize} > {bufferSize}), not sending any further" +
-                    "metrics for interval. Increase metrics buffer using client config option if required. ", cacheSize,
-                    bufferSize);
+                var globalTarget = new Target(EvaluationAnalytics.GlobalTargetIdentifier,
+                    EvaluationAnalytics.GlobalTargetName, null);
+                PushToEvaluationAnalyticsCache(featureConfig, variation, globalTarget, cacheSize, bufferSize);
             }
             else
             {
-                if (isGlobalTargetEnabled)
-                {
-                    var globalTarget = new Target(EvaluationAnalytics.GlobalTargetIdentifier,
-                        EvaluationAnalytics.GlobalTargetName, null);
-                    PushToEvaluationAnalyticsCache(featureConfig, variation, globalTarget);
-                }
-                else
-                {
-                    PushToEvaluationAnalyticsCache(featureConfig, variation, target);
-                }
-
-                // Create target metrics 
-                PushToTargetAnalyticsCache(target);
-
+                PushToEvaluationAnalyticsCache(featureConfig, variation, target, cacheSize, bufferSize);
             }
+
+            // Create target metrics 
+            PushToTargetAnalyticsCache(target, cacheSize, bufferSize);
         }
 
-        private void PushToEvaluationAnalyticsCache(FeatureConfig featureConfig, Variation variation, Target target)
+        private void PushToEvaluationAnalyticsCache(FeatureConfig featureConfig, Variation variation, Target target,
+            int cacheSize, int bufferSize)
         {
             Analytics evaluationAnalytics = new EvaluationAnalytics(featureConfig, variation, target);
+
             var evaluationCount = analyticsCache.getIfPresent(evaluationAnalytics);
+
+            // Cache is full and this is new entry, so we should discard this eval
+            if (cacheSize > bufferSize && evaluationCount == 0)
+            {
+                LogMetricsIgnoredWarning(cacheSize, bufferSize);
+                return;
+            }
+            
+            // If cache isn't full, or if there is an existing matching key, add this one
             analyticsCache.Put(evaluationAnalytics, evaluationCount + 1);
         }
-        
-        private void PushToTargetAnalyticsCache(Target target)
+
+
+        private void PushToTargetAnalyticsCache(Target target, int cacheSize, int bufferSize)
         {
+            //  Cache is full, discard target
+            if (cacheSize > bufferSize)
+            {
+                LogMetricsIgnoredWarning(cacheSize, bufferSize);
+                return;
+            }
+
             Analytics targetAnalytics = new TargetAnalytics(target);
-            
+
             // We don't need to keep count of targets, so use a constant value, 1, for the count. 
             // Since 1.4.2, the analytics cache was refactored to separate out Evaluation and Target metrics, but the 
             // change did not go as far as to maintain two caches (due to effort involved), but differentiate them based on subclassing, so 
             // the counter used for target metrics isn't needed, but causes no issue. 
             analyticsCache.Put(targetAnalytics, 1);
+        }
+
+        private void LogMetricsIgnoredWarning(int cacheSize, int bufferSize)
+        {
+            // Only log this once per interval
+            if (warningLoggedForInterval)
+            {
+                return;
+            }
+            
+            logger.LogWarning(
+                "Metric frequency map exceeded buffer size ({cacheSize} > {bufferSize}), not sending any further" +
+                "metrics for interval. Increase metrics buffer using client config option if required. ",
+                cacheSize,
+                bufferSize);
+            
+            warningLoggedForInterval = true;
         }
 
         internal void Timer_Elapsed(object sender, ElapsedEventArgs e)
@@ -111,6 +137,7 @@ namespace io.harness.cfsdk.client.api.analytics
             try
             {
                 analyticsPublisherService.SendDataAndResetCache();
+                warningLoggedForInterval = false;
             }
             catch (CfClientException ex)
             {
