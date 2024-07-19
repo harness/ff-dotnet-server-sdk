2dbd6a697e7aa676fd1f694fba09f907dfb6d793
Erdi Rowlands <erdirowlands@gmail.com>
2024-07-19 15:49:58 +0100

    FFM-11759 Fix build script

--- build.sh
+++ build.sh
@@ -16,14 +16,21 @@ dotnet tool install --global coverlet.console --version 3.2.0
 dotnet tool install --global dotnet-reportgenerator-globaltool
 dotnet tool restore
 
-# Install Libraries needed for build and buld
+# Install Libraries needed for build and build
 dotnet restore ff-netF48-server-sdk.csproj
 dotnet build ff-netF48-server-sdk.csproj --no-restore
-
 
 # Run tests
 echo "Generating Test Report"
 export MSBUILDDISABLENODEREUSE=1
 dotnet test tests/ff-server-sdk-test/ff-server-sdk-test.csproj -v=n --blame-hang --logger:"junit;LogFilePath=junit.xml" -nodereuse:false
+
+# Capture exit code of the test command
+TEST_EXIT_CODE=$?
+if [ "$TEST_EXIT_CODE" -ne 0 ]; then
+  echo "Tests failed. Aborting build."
+  exit $TEST_EXIT_CODE
+fi
+
 ls -l
 echo "Done"
